347bd8a38c8e3d4fdbf42166e8356195f8b62f1e
Timothy <tbirrell@fahrenheit.io>
2016-09-12 12:57:38 -0500

    fixed board instatiation
    corrected board orientation
    refresh board after move to fix the castling bug

--- public/js/game.js
+++ public/js/game.js
@@ -9,7 +9,7 @@ var gamePath = window.location.pathname;
 
 $.get(gamePath + '/findGame', function(data) {
   var playerHash = gamePath.substr(-5);
-  
+
   //configure board orientation
   if (playerHash == data[0].white) {
     player = {
@@ -30,9 +30,65 @@ $.get(gamePath + '/findGame', function(data) {
   setUpGame(data[0].fen, player);
 });
 
-function setUpGame(fen, player) {
-  //logic
-  chess = new Chess(fen);
+//functions for board instatiation
+var onDragStart = function(source, piece, position, orientation) {
+  // do not pick up pieces if the game is over
+  // only pick up pieces for the side to move
+  if (chess.game_over() === true ||
+     (chess.turn() === 'w' && piece.search(/^b/) !== -1) ||
+     (chess.turn() === 'b' && piece.search(/^w/) !== -1) ||
+     (chess.turn() != player.abbr)) {
+    return false;
+  }
+}
+var onDrop = function(source, target, peice, newPos, oldPos, orientation){
+  var move = chess.move({
+    from: source,
+    to: target,
+    promotion: 'q'
+  });
+
+  console.log(move);
+
+  if (move === null) return 'snapback'
+
+  game = checkGameStatus();
+
+  var humanMove = move.from + '-' + move.to;
+
+  update(humanMove);
+}
+var onMouseoverSquare = function(square, piece) {
+  // get list of possible moves for this square
+  var moves = chess.moves({
+    square: square,
+    verbose: true
+  });
+
+  // exit if there are no moves available for this square
+  // exit if not your turn
+  if (chess.turn() != player.abbr) return;
+  if (moves.length === 0) return;
+
+  // highlight the square they moused over
+  greySquare(square);
+
+  // highlight the possible squares for this piece
+  for (var i = 0; i < moves.length; i++) {
+    greySquare(moves[i].to);
+  }
+}
+var onMouseoutSquare = function(square, piece) {
+  //remove grey squares
+  $('#board .square-55d63').css('background', '');
+}
+
+// instantiate game
+function setUpGame(fen, player, update = false) {
+  if (!update) {
+    //logic
+    chess = new Chess(fen);
+  }
 
   var drag = false;
   if (chess.turn() == player.abbr) drag = true;
@@ -43,58 +99,11 @@ function setUpGame(fen, player) {
     dropOffBoard: 'snapback',
     draggable: drag,
     showNotation: false,
-    orientation: player,
-    onDragStart: function(source, piece, position, orientation) {
-      // do not pick up pieces if the game is over
-      // only pick up pieces for the side to move
-      if (chess.game_over() === true ||
-         (chess.turn() === 'w' && piece.search(/^b/) !== -1) ||
-         (chess.turn() === 'b' && piece.search(/^w/) !== -1) ||
-         (chess.turn() != player.abbr)) {
-        return false;
-      }
-    },
-    onDrop: function(source, target, peice, newPos, oldPos, orientation){
-      var move = chess.move({
-        from: source,
-        to: target,
-        promotion: 'q'
-      });
-
-      console.log(move);
-
-      if (move === null) return 'snapback'
-
-      game = checkGameStatus();
-
-      var humanMove = move.from + '-' + move.to;
-
-      update(humanMove);
-    },
-    onMouseoverSquare: function(square, piece) {
-      // get list of possible moves for this square
-      var moves = chess.moves({
-        square: square,
-        verbose: true
-      });
-
-      // exit if there are no moves available for this square
-      // exit if not your turn
-      if (chess.turn() != player.abbr) return;
-      if (moves.length === 0) return;
-
-      // highlight the square they moused over
-      greySquare(square);
-
-      // highlight the possible squares for this piece
-      for (var i = 0; i < moves.length; i++) {
-        greySquare(moves[i].to);
-      }
-    },
-    onMouseoutSquare: function(square, piece) {
-      //remove grey squares
-      $('#board .square-55d63').css('background', '');
-    }
+    orientation: player.full,
+    onDragStart: onDragStart,
+    onDrop: onDrop,
+    onMouseoverSquare: onMouseoverSquare,
+    onMouseoutSquare: onMouseoutSquare
   });
 }
 
@@ -114,19 +123,15 @@ function checkGameStatus() {
     return 'check';
   }
   if (chess.in_checkmate()) {
-    board.draggable = false;
     return 'checkmate';
   }
   if (chess.in_draw()) {
-    board.draggable = false;
     return 'draw';
   }
   if (chess.in_stalemate()) {
-    board.draggable = false;
     return 'stalemate';
   }
   if (chess.insufficient_material()) {
-    board.draggable = false;
     return 'draw';
   }
   return 'inprogress';
@@ -143,6 +148,7 @@ function update(lastmove) {
     },
     success: function(msg){
       console.log(msg);
+      setUpGame(chess.fen(), player, true);
     }
   });
 }
